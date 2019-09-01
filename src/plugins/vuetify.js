6950fe8f987724de768495354c5a1e2c7c06fdd3
radcki <radekadamczewski@gmail.com>
2019-09-01 12:13:08 +0200

    remove unused plugin

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify/lib'
+import /* webpackPrefetch: true */ Vuetify from 'vuetify/lib'
 import pl from 'vuetify/es5/locale/pl'
 import en from 'vuetify/es5/locale/en'
 
